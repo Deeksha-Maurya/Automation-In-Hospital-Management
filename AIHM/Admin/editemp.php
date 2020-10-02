10efbcfc8c7efec3328d20a5dd3c7bcc06952092
Deeksha-Maurya <72131643+Deeksha-Maurya@users.noreply.github.com>
2020-10-02 18:06:44 +0530

    Update editemp.php

--- AIHM/Admin/editemp.php
+++ AIHM/Admin/editemp.php
@@ -14,7 +14,7 @@ session_start();
   // Checking for Empty Fields
   if(($_REQUEST['empName'] == "") || ($_REQUEST['empCity'] == "") || ($_REQUEST['empMobile'] == "") || ($_REQUEST['empEmail'] == "")){
    // msg displayed if required field missing
-   $msg = '<div class="alert alert-warning col-sm-6 ml-5 mt-2" role="alert"> Fill All Fileds </div>';
+   $msg = '<div class="alert alert-warning col-sm-6 ml-5 mt-2" role="alert"> Fill All Fields </div>';
   } else {
     // Assigning User Values to Variable
     $eId = $_REQUEST['empId'];
