3f4a09fdb9125ad2e2b11adf4811f7886b201762
Deeksha-Maurya <72131643+Deeksha-Maurya@users.noreply.github.com>
2020-10-02 18:07:50 +0530

    Update editreq.php

--- AIHM/Admin/editreq.php
+++ AIHM/Admin/editreq.php
@@ -14,7 +14,7 @@ session_start();
   // Checking for Empty Fields
   if(($_REQUEST['r_login_id'] == "") || ($_REQUEST['r_name'] == "") || ($_REQUEST['r_email'] == "")){
    // msg displayed if required field missing
-   $msg = '<div class="alert alert-warning col-sm-6 ml-5 mt-2" role="alert"> Fill All Fileds </div>';
+   $msg = '<div class="alert alert-warning col-sm-6 ml-5 mt-2" role="alert"> Fill All Fields </div>';
   } else {
     // Assigning User Values to Variable
     $rid = $_REQUEST['r_login_id'];
